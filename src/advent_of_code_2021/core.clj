43e68fc1cfe826b47f911c6fedd0998b7a1a511b
Dom Parfitt <mail@domparfitt.com>
2021-12-05 16:07:25 +0000

    Update main runner

--- src/advent_of_code_2021/core.clj
+++ src/advent_of_code_2021/core.clj
@@ -1,10 +1,16 @@
 (ns advent-of-code-2021.core
-  (:gen-class))
+  (:gen-class)
+  (:require [advent-of-code-2021.day-1 :as day-1]
+            [clojure.string :refer [upper-case]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (if (seq args)
     (doseq [arg args]
-      (println arg))
+      (println (upper-case arg))
+      (case arg
+        "day-1" (day-1/run)
+        (println "Not found"))
+      (println))
     (println "Nope")))
